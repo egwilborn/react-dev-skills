5284008b12e662fa8f22e0d4bdf5db5aa6744a74
egwilborn <egwilborn@gmail.com>
2023-07-17 11:29:42 -0500

    sets up list - part one

---
+++ src/NewSkillForm.jsx
@@ -0,0 +1,17 @@
+export default function NewSkillForm() {
+  return (
+    <form className="NewSkillForm">
+      <label>Skill</label>
+      <input></input>
+      <label>Level</label>
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button>ADD Skill</button>
+    </form>
+  );
+}
---
+++ src/SkillsList.jsx
@@ -0,0 +1,11 @@
+import SkillListItem from "./SkillListItem";
+
+export default function SkillsList() {
+  return (
+    <ul className="SkillsList">
+      <SkillListItem />
+      <SkillListItem />
+      <SkillListItem />
+    </ul>
+  );
+}
